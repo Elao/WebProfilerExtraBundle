c68cc346d5e4267c406f2801b0c1210480af092b
Vbouzeran <vincent.bouzeran@elao.com>
2011-05-09 12:03:44 +0200

    Update container collector (remove lazy loading, using Ryan container debug command stuff)

--- DataCollector/ContainerDataCollector.php
+++ DataCollector/ContainerDataCollector.php
@@ -5,6 +5,9 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ * 
+ * 
+ * 
  */
 
 namespace Elao\WebProfilerExtraBundle\DataCollector;
@@ -15,9 +18,11 @@ use Symfony\Component\HttpKernel\DataCollector\DataCollector;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
-use Symfony\Component\DependencyInjection\ParameterBag\FrozenParameterBag;
+use Symfony\Component\DependencyInjection\Alias;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
 
 /**
  * ContainerDataCollector.
@@ -27,10 +32,15 @@ use Symfony\Component\DependencyInjection\ParameterBag\FrozenParameterBag;
 class ContainerDataCollector extends DataCollector
 {
     protected $kernel;
-
+    protected $container;
+	protected $containerBuilder;
+	
+    
     public function __construct(Kernel $kernel)
     {
-        $this->kernel = $kernel;
+        $this->kernel    		= $kernel;
+        $this->container 		= $kernel->getContainer();
+        $this->containerBuilder = $this->getContainerBuilder();
     }
 
     public function getKernel()
@@ -43,12 +53,11 @@ class ContainerDataCollector extends DataCollector
      */
     public function collect(Request $request, Response $response, \Exception $exception = null)
     {
-        $kernel = $this->getKernel();
-        $container = $kernel->getContainer();
-        $_parameters = $container->parameters;        
+        $serviceIds = $this->containerBuilder->getServiceIds();
         $parameters = array();
+        $services   = array();
         
-        foreach ($_parameters as $key => $value)
+        foreach ($this->containerBuilder->getParameterBag()->all() as $key => $value)
         {
             $service = substr($key, 0, strpos($key, '.'));
             if (!isset($parameters[$service]))
@@ -58,23 +67,25 @@ class ContainerDataCollector extends DataCollector
             $parameters[$service][$key] = $value;
         }
         
-        $services   = array();
-        $serviceIds = $container->getServiceIds();
-        
-        $this->data = array();
-        
         foreach ($serviceIds as $serviceId)
         {
-            $services[$serviceId] = array(
-                'service_id'  => $serviceId,
-                'class'       => get_class($container->get($serviceId)) 
-            );
+			$definition = $this->resolveServiceDefinition($serviceId);
+			
+			if ($definition instanceof Definition && $definition->isPublic()) {
+				$services[$serviceId] = array('class' => $definition->getClass(), 'scope' => $definition->getScope());
+	            $scope = $definition->getScope();
+	            $class = $definition->getClass();
+			} elseif ($definition instanceof Alias) {
+				$services[$serviceId] = array('alias' => $definition);
+	        } else {
+	        	continue;	// We don't want private services 
+			}
         }
+        
         ksort($services);
         ksort($parameters);
         $this->data['parameters'] = $parameters;
-        
-        $this->data['services'] = $services;
+        $this->data['services']   = $services;
     }
     
     public function getParameters()
@@ -91,6 +102,55 @@ class ContainerDataCollector extends DataCollector
     {
         return $this->data['services'];
     }
+    
+
+
+    /**
+	* Loads the ContainerBuilder from the cache.
+	* @author Ryan Weaver <ryan@thatsquality.com>
+	* @return ContainerBuilder
+	*/
+    private function getContainerBuilder()
+    {
+        if (!$this->getKernel()->isDebug()) {
+            throw new \LogicException(sprintf('Debug information about the container is only available in debug mode.'));
+        }
+
+        if (!file_exists($cachedFile = $this->container->getParameter('debug.container.dump'))) {
+            throw new \LogicException(sprintf('Debug information about the container could not be found. Please clear the cache and try again.'));
+        }
+
+        $container = new ContainerBuilder();
+
+        $loader = new XmlFileLoader($container, new FileLocator());
+        $loader->load($cachedFile);
+
+        return $container;
+    }
+
+    /**
+    * @author Ryan Weaver <ryan@thatsquality.com>
+	* Given an array of service IDs, this returns the array of corresponding
+	* Definition and Alias objects that those ids represent.
+	*
+	* @param string $serviceId The service id to resolve
+	* @return \Symfony\Component\DependencyInjection\Definition|\Symfony\Component\DependencyInjection\Alias
+	*/
+    private function resolveServiceDefinition($serviceId)
+    {
+        if ($this->containerBuilder->hasDefinition($serviceId)) {
+            return $this->containerBuilder->getDefinition($serviceId);
+        }
+
+        // Some service IDs don't have a Definition, they're simply an Alias
+        if ($this->containerBuilder->hasAlias($serviceId)) {
+            return $this->containerBuilder->getAlias($serviceId);
+        }
+
+        // the service has been injected in some special way, just return the service
+        return $this->containerBuilder->get($serviceId);
+    }
+    
     
     /**
      * {@inheritdoc}
