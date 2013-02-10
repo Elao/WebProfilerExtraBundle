93cdc1b4adacfeff74eb501c7eed140ae8fc78e3
Grégoire Pineau <lyrixx@lyrixx.info>
2013-02-10 12:14:59 +0100

    Removed dead code (again)

--- WebProfilerExtraBundle.php
+++ WebProfilerExtraBundle.php
@@ -6,13 +6,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class WebProfilerExtraBundle extends Bundle
 {
-    public function getNamespace()
-    {
-        return __NAMESPACE__;
-    }
-
-    public function getPath()
-    {
-        return strtr(__DIR__, '\\', '/');
-    }
 }
